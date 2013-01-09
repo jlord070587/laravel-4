d96274200f2e3a398f94c3d00b7d09d658ad300f
Davide Bellini <dbellini@freetechgroup.com>
2013-01-09 18:37:05 +0100

    Kint test for var debug

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,35 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	$testVar = array(
+		'var1' => array(
+			'subvar1' => array(
+				'subsubvar1' => array(
+					'subsubsubvar1' => array(
+						'Hey'
+					),
+				),
+			),
+		),
+		'var2' => array(
+			'subvar2' => array(
+				'subsubvar2' => array(
+					'subsubsubvar2' => array(
+						'Hey'
+					),
+				),
+			),
+		),
+		'var3' => array(
+			'subvar3' => array(
+				'subsubvar3' => array(
+					'subsubsubvar3' => array(
+						'Hey'
+					),
+				),
+			),
+		),
+	);
+
+	return View::make('hello')->with('testVar', $testVar);
 });
